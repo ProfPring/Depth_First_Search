b184584a8a135aa0b7d10ab06fb5abc63c805b01
unknown <samrobbo1997@live.co.uk>
2022-10-02 18:01:20 +0100

    first commit

---
+++ Depth-First Search/aglorithems.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Depth_First_Search
+{
+    public class aglorithems
+    {
+        public HashSet<T> DFS<T>(Graph<T> graph, T start)
+        {
+            var vivisted = new HashSet<T>();
+
+            if (!graph.AdjacencyList.ContainsKey(start))
+            {
+                return vivisted;
+            }
+
+            var stack = new Stack<T>();
+            stack.Push(start);
+
+            while (stack.Count > 0)
+            {
+                var vertex = stack.Pop();
+
+                if (vivisted.Contains(vertex))
+                {
+                    continue;
+                }
+                vivisted.Add(vertex);
+
+                foreach (var neighbor in graph.AdjacencyList[vertex])
+                {
+                    if (!vivisted.Contains(neighbor))
+                    {
+                        stack.Push(neighbor);
+                    }
+                }
+            }
+
+           return vivisted;
+        }
+    }
+}
---
+++ Graph.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class graph<T>
+{
+	public graph()
+	{
+	}
+
+	public graph(IEnumerable<T> vertices, IEnumerable<Tuple<T, T>> edges)
+	{
+		foreach (var vertex in vertices) 
+		{
+			AddVertex(vertex);
+		}
+
+		foreach (var edge in edges)
+		{
+			AddEdge(edge;
+		}
+	}
+
+
+	public Dictionary<T, HashSet<T>> AdjacencyList { get; } = new Dictionary<T, HashSet<T>>();
+
+	public void AddVertex(T Vertex) 
+	{
+		AdjacencyList[vertex] = new HashSet<T>();
+	}
+
+	public void AddEdge(Tuple<T, T> edge)
+	{
+		if (AdjacencyList.ContainsKey(edge.Item1) && AdjacencyList.ContainsKey(edge.Item2))
+		{
+			AdjacencyList[edge.Item1].Add(edge.Item2);
+			AdjacencyList[edge.Item2].Add(edge.Item1);
+		}
+	}
+}
